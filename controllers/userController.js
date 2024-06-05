9353eafc96d7b66fc9055dd3e621de1e364b2a38
AlvinSet <alvingm17@gmail.com>
2024-06-05 13:10:31 -0300

    install bcrypt and jsonwebtoken, add login to user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,18 +1,59 @@
 import User from '../models/userModel.js';
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 // Crear un nuevo usuario
+
+const salt= 10;
+const secreyKey = 'appHotel';
+
 async function createUser( req, res  ){
     try {
-        const newUser = new User(req.body);
+        const {name, email, phone, password} = req.body;
+
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            name: name,
+            email: email,
+            phone: phone,
+            password: passwordHash
+        });
+
         await newUser.save();
 
-        res.status(201).json({ newUser});
+        res.status(201).json({ newUser: { name: newUser.name}});
 
     } catch (error) {
         console.error(error);
         res.status(400).json({ message: error, data: []});
     }
 }
+
+
+async function login (req, res){
+    try {
+        const {email, password} = req.body;
+
+        const user = await User.findOne({email});
+
+        if(!user){
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        const pass = await bcrypt.compare(password, user.password);
+        if (!pass) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+        const token = jwt.sign({userId: user._id, email: user.email},secretKey, {expiresIn: 'ih'});
+        res.status(200).json({ message: 'ok', data: {token: token}});
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Server error" });
+    }
+}
+
 
 // Obtener todos los usuarios
 async function getAllUsers  (req, res){
@@ -39,12 +80,17 @@ async function getUserById (req, res){
 
 //actualizar un usuario
 async function updateUser (req, res){
+    const { password } = req.body;
+    if (password) {
+        req.body.password = await bcrypt.hash(password, 10);
+    }
+    
 try {
     const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true } );
 if (!user){
     return res.status(404).send("User not found.")
 }
-res.status(200).json(user);
+res.status(200).json({ name: user.name, email: user.email});
 
 } catch (error) {
     res.status(400).send(error.message);
--- models/userModel.js
+++ models/userModel.js
@@ -12,6 +12,11 @@ const userSchema = new mongoose.Schema({
             trim: true,
             lowercase: true,
             match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']},
+    password: {
+        type: String,
+    required: [true, "Password is required"],
+    minlength: [6, "Password must be at least 6 characters long"]
+    },
     phone: {
         type: String,
         required: [true, "Phone number is required"],
